89c234e71fef6d28a8e1b97c6ee4bc124f3fcfa1
Brian Anderson <andersrb@gmail.com>
2010-06-29 00:19:56 -0400

    Introduce quickshake dependency

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -6,5 +6,6 @@ class AndroidPlugin(info: ProjectInfo) extends PluginProject(info)
 	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 	Credentials(Path.fromFile(System.getProperty("user.home")) / ".ivy2" / ".credentials", log)
 	
+	val quickshake = "net.negatory" % "quickshake_2.7.7" % "0.1-SNAPSHOT"
 	val proguard = "net.sf.proguard" % "proguard" % "4.4"
 }
